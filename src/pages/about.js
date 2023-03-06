5fb7fc895863cf273ed2a193f194e49e781a2bfd
Matthew Moniz <Matt@Fastforward.sh>
2023-03-06 13:28:23 -0500

    finished tutorial part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,15 +7,6 @@ import Layout from '../components/layout'
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    // <main>
-    //   <title>About Me</title>
-    //   <h1>About Me</h1>
-    //   <Link to="/">Back to Home</Link>
-    //   <p>
-    //     Hi there! I'm the proud creator of this site, which I built with Gatsby.
-    //   </p>
-    // </main>
-
     <Layout pageTitle="About Me">
       <p>
         Hi there! I'm the proud creator of this site, which I built with Gatsby.
